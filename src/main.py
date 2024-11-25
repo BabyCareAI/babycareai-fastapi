795f7d0917c101df0fcc3fde7eb10b8489ea7655
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2024-11-25 22:06:49 +0900

    refactor: redis stream 관련 코드 삭제

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import asyncio
 from fastapi import FastAPI
 from guide.router import router as nlp_router
 from guide.service import initialize_service
-from guide.redis_listener import redis_stream_listener
 
 app = FastAPI(root_path="/fastapi")
 
@@ -11,4 +10,3 @@ app.include_router(nlp_router)
 @app.on_event("startup")
 async def startup_event():
     await initialize_service()
-    asyncio.create_task(redis_stream_listener())
