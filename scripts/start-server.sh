356a0717078efafdde62469f2eeae8e05b02685e
BaxDailyGit <tmdwls8387@ajou.ac.kr>
2025-05-30 18:41:20 +0900

    chore: 배포 버전 업데이트, 배포 시작 메시지 수정 (release/40-v0.4.2 배포) #40

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "--------------- release/37-v0.4.1 배포 시작(2) -----------------"
+echo "--------------- release/37-v0.4.2 배포 시작 -----------------"
 
 cd /home/ubuntu/babycareai-server || {
   echo "디렉토리 이동 실패: /home/ubuntu/babycareai-server"
